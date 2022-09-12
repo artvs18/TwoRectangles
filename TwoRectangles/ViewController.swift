7f90262319e1df57cde1d14d528912216e73ce5d
Artem Volkov <wasartemwolf@gmail.com>
2022-09-12 23:47:55 +0300

    Initial Commit

---
+++ TwoRectangles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoRectangles
+//
+//  Created by Artemy Volkov on 12.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
